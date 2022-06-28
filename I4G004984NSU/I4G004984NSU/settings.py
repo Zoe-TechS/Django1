0a9d9c7e753ced7c923751ff5dee8e1c80851849
unknown <osatoama@gmail.com>
2022-06-28 10:09:03 +0100

    No_Secret

--- I4G004984NSU/I4G004984NSU/settings.py
+++ I4G004984NSU/I4G004984NSU/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = 'django-insecure-ad64!w956=vi3(6qi#j&y$qmctn*y8$92pcny3&2=0ye3_53_7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
